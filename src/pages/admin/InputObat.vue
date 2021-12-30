b7c146d73d7e6cb509b4a2378ab88ac6a9720227
erikozakaria <erikomedia123@gmail.com>
2021-12-30 13:37:21 +0700

    apotikfrontend

---
+++ src/pages/admin/InputObat.vue
@@ -0,0 +1,117 @@
+<template>
+    <q-page padding>
+        <div class="row q-mb-md col-gutter-md">
+      <div class="col-md-12 col-xs-12 col-lg-12">
+        <div class="row">
+          <div class="col-auto">
+            <div class="left blue"></div>
+          </div>
+          <div class="col">
+            <q-banner inline-actions class="text-blue-grey-14">
+              <div class="text-h6">Input Obat</div>
+              <div>Input Data Obat Baru</div>
+            </q-banner>
+          </div>
+        </div>
+      </div>
+    </div>
+    <q-card flat>
+      <q-card-section class="row">
+        <q-form
+          @submit="onSubmit()"
+            class="q-col-gutter-md q-col-lg-6 col-md-6 col-xs-12"
+            >
+            <q-input
+                filled
+                v-model="form.namaObat"
+                label="Nama Obat"
+                :rules="[ val => val && val.length > 0 || 'Mohon Isi Nama Obat']"
+            />
+            <q-input
+                filled
+                v-model="form.harga"
+                type="number"
+                label="Harga"
+                :rules="[ val => val && val.length > 0 || 'Mohon Isi Harga']"
+            />
+            <q-input
+                filled
+                v-model="form.tahun"
+                label="Expired"
+                :rules="[ val => val && val.length > 0 || 'Mohon Isi Expired']"
+            />
+
+            <q-select
+              filled
+              v-model="form.golongan"
+              :options="optionGolongan"
+              label="Pilih Golongan"
+            />
+
+            <q-input
+              v-model="form.deskripsi"
+              filled
+              type="textarea"
+              label="Deskripsi"
+            />
+
+            <q-file accept=".jpg, image/*" color="teal" filled v-model="image" label="Upload Gambar">
+              <template v-slot:prepend>
+                <q-icon name="cloud_upload" />
+              </template>
+            </q-file>
+
+            <div>
+                <q-btn label="Submit" type="submit" color="primary"/>
+                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            </div>
+            </q-form>
+      </q-card-section>
+    </q-card>
+    </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        namaObat: null,
+        harga: 0,
+        tahun: null,
+        golongan: null,
+        rating: 0,
+        deskripsi: null
+      },
+      optionGolongan: [
+        'Golongan Beta Lactams',
+        'Golongan Cell Membrane',
+        'Golongan Folate Synthesis'
+      ],
+      image: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      const formData = new FormData()
+      formData.append('image', this.image)
+      formData.append('data', JSON.stringify(this.form))
+      this.$axios.post('obat/insert', formData)
+        .then(res => {
+          if (res.data.sukses) {
+            this.$showNotif(res.data.pesan, 'positive')
+            this.$router.push({ name: 'dataObat' })
+          } else {
+            this.$showNotif(res.data.pesan, 'negative')
+          }
+        })
+    }
+  }
+}
+</script>
+<style scoped>
+  .left {
+    width: 5px;
+    height: 100%;
+    background: aqua;;
+  }
+</style>
---
+++ src/pages/admin/Tentang.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-page padding>
+      <div class="row q-mb-md col-gutter-md">
+      <div class="col-md-12 col-xs-12 col-lg-12">
+        <div class="row">
+          <div class="col-auto">
+            <div class="left blue"></div>
+          </div>
+          <div class="col">
+            <q-banner inline-actions class="text-blue-grey-14">
+              <div class="text-h6">Apotik Sederhana</div>
+              <div>Dibuat Oleh</div>
+            </q-banner>
+          </div>
+        </div>
+      </div>
+    </div>
+    <q-card flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="statics/eriko.jpg">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Eriko Zakaria</q-item-label>
+          <q-item-label caption>
+            18421003
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator />
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ info }}
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section class="col-4">
+          <q-item-label>Ig : erikozakaria</q-item-label>
+          <q-item-label>WA : 0895606009940 </q-item-label>
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+    <div class="row q-mb-md col-gutter-md">
+      <div class="col-md-12 col-xs-12 col-lg-12">
+        <div class="row">
+          <div class="col-auto">
+            <div class="left blue"></div>
+          </div>
+          <div class="col">
+          </div>
+        </div>
+      </div>
+    </div>
+    <q-card flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="statics/kasino.jpeg">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Ahmad Rofiq Zulkarnain </q-item-label>
+          <q-item-label caption>
+            18421064
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator />
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ info }}
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section class="col-4">
+          <q-item-label>Ig : ahmadrofiqzulkarnain </q-item-label>
+          <q-item-label>WA : 082306839842 </q-item-label>
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      info: 'Mahasiswa Fakultas Ilmu Komputer Universitas Bandar Lampung 2018'
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+</style>
+<style scoped>
+  .left {
+    width: 5px;
+    height: 100%;
+    background: pink;
+  }
+</style>
